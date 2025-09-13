9988585dd9fd82581463bb5b0d788192d692774c
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-09-13 19:04:25 +0200

    feat(pages): add login page

---
+++ src/pages/login.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import type { FormSubmitEvent } from "@nuxt/ui";
+import * as z from "zod";
+
+const fields = [
+  {
+    name: "email",
+    label: "Email address",
+    type: "text",
+  },
+  {
+    name: "password",
+    label: "Password",
+    type: "password",
+  },
+];
+
+const schema = z.object({
+  email: z.string("Invalid email"),
+  password: z.string("Password is required"),
+});
+
+type Schema = z.output<typeof schema>;
+
+const onSubmit = (event: FormSubmitEvent<Schema>) => {
+  console.log(event.data);
+};
+</script>
+
+<template>
+  <AuthForm :fields :schema @submit="onSubmit">
+    <template #title>
+      <span>📚</span>
+      <br />
+      <span>Log in account</span>
+    </template>
+
+    <template #subtitle>
+      Enter your email and password below to log in
+    </template>
+
+    <template #submitText>Log in</template>
+
+    <template #additionnal>
+      <UButton to="/register" variant="link" block>
+        Don't have an account ? Sign up
+      </UButton>
+    </template>
+  </AuthForm>
+</template>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,4 +6,9 @@ export const routes: RouterOptions["routes"] = [
     name: "register",
     component: () => import("@/pages/register.vue"),
   },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("@/pages/login.vue"),
+  },
 ];
